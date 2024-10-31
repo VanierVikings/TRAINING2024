2e0b25748f2f485d15025debe316c47095076ee0
Arjun <62486112+arjunalwe@users.noreply.github.com>
2024-10-31 15:16:27 -0400

    wrbpinrgb[nurgb[no

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,12 @@
 package frc.robot;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 
 public class RobotContainer {
         public final static CommandXboxController mDriver = new CommandXboxController(0);
+        public final static Intake m_intake = new Intake();
+        public final static Shooter m_shooter = new Shooter();
         public RobotContainer() {
         configureBindings();
     }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,11 +3,19 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
+    DigitalInput beamBreak = new DigitalInput(9);
+
     public Intake() {
     }
+
+    public Boolean hasNote() {
+        return !beamBreak.get();
+    }
+
 }
 
